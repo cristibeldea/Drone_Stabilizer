bb97f98455fc40154a78a153d420db186a1ad7e6
Bogdan Beldea <beldeabogdan@gmail.com>
2024-12-18 18:52:24 +0200

    WIP

--- Environment.py
+++ Environment.py
@@ -16,17 +16,17 @@ wind_range = max_wind_force - min_wind_force
 point = (0, 0)
 FPS = 60
 
-kp_x = 0
-ki_x = 0
-kd_x = 0
+kp_x = -2
+ki_x = -3
+kd_x = -50
 
 kp_y = 25  # Slightly reduced to lessen overshoot.
 ki_y = 30  # Integral term stays moderate to counter drift.
 kd_y = 500   # Increased damping to better control oscillations.
 
-kp_angle = 25   # Increase proportional term to address the tilt more aggressively.
-ki_angle = 30  # A tiny integral term to handle persistent tilt offsets.
-kd_angle = 500   # Strong damping to suppress oscillations in angular motion.
+kp_angle = -500   # Increase proportional term to address the tilt more aggressively.
+ki_angle = -400  # A tiny integral term to handle persistent tilt offsets.
+kd_angle = -2500   # Strong damping to suppress oscillations in angular motion.
 
 
 
@@ -97,38 +97,38 @@ def generateForces():
 
 def PID_control_x(drone, target_position, errors_x):
     current_x = drone.position.x
-    for i in range(1, 3):
+    for i in range(2, 0, -1):
         errors_x[i] = errors_x[i - 1]
     errors_x[0] = target_position[0] - current_x
-    #TODO errors_x[0] = current_x - target_position[0]
 
-    PID_x = kp_x * errors_x[0] + ki_x * (errors_x[0] + errors_x[1] + errors_x[2]) + kd_x * (errors_x[0] - errors_x[2])
-    return clamp(PID_x, -8000, 0)
+    clamped_errors_x = [clamp(i, -100, 100) for i in errors_x]
+    PID_x = kp_x * clamped_errors_x[0] + ki_x * (clamped_errors_x[0] + clamped_errors_x[1] + clamped_errors_x[2]) + kd_x * (clamped_errors_x[0] - clamped_errors_x[2])
+    return PID_x
 
 
 def PID_control_y(drone, target_position, errors_y):
     current_y = drone.position.y
     gravity_compensation = 981 * 8
-    for i in range(1, 3):
+    for i in range(2, 0, -1):
         errors_y[i] = errors_y[i - 1]
     errors_y[0] = target_position[1] - current_y
-    #TODO errors_y[0] = current_y - target_position[1]
 
-    PID_y = kp_y * errors_y[0] + ki_y * (errors_y[0] + errors_y[1] + errors_y[2]) + kd_y * (errors_y[0] - errors_y[2])
-    return clamp(PID_y, -8000, 0)
+    clamped_errors_y = [clamp(i, -100, 100) for i in errors_y]
+    PID_y = kp_y * clamped_errors_y[0] + ki_y * (clamped_errors_y[0] + clamped_errors_y[1] + clamped_errors_y[2]) + kd_y * (clamped_errors_y[0] - clamped_errors_y[2])
+
+    return PID_y
 
 
 def PID_control_angle(drone, target_angle, errors_angle):
     current_angle = drone.angle
-    for i in range(1, 3):
+    for i in range(2, 0, -1):
         errors_angle[i] = errors_angle[i - 1]
 
     errors_angle[0] = target_angle - current_angle
-    #TODO errors_angle[0] = current_angle - target_angle
 
     PID_angle = kp_angle * errors_angle[0] + ki_angle * (
                 errors_angle[0] + errors_angle[1] + errors_angle[2]) + kd_angle * (errors_angle[0] - errors_angle[2])
-    return clamp(PID_angle, -8000, 0)
+    return PID_angle
 
 
 def main():
@@ -215,7 +215,10 @@ def main():
         thruster_left = pid_ctrl_y_output + pid_ctrl_x_output + pid_ctrl_angle_output
         thruster_right = pid_ctrl_y_output - pid_ctrl_x_output - pid_ctrl_angle_output
 
-        print(thruster_left, thruster_right)
+        thruster_left = clamp(thruster_left, -8000, 0)
+        thruster_right = clamp(thruster_right, -8000, 0)
+
+        print(f"L:{thruster_left:4.1f} R:{thruster_right:4.1f} x:{errors_x[0]:4.1f} y:{errors_y[0]:4.1f} w:{errors_angle[0]:4.1f}")
 
         drone.apply_force_at_local_point((0, thruster_left), (-50, 0))
         drone.apply_force_at_local_point((0, thruster_right), (50, 0))
@@ -229,9 +232,9 @@ def main():
         #     drone.apply_force_at_local_point((0, reverse * 1 / modelCorrection * -5000.0),
         #                                      (50, 0))  # propulsor pe dreapta
 
-        # TODO
+        # TODO WIND
         elapsed_time = (pygame.time.get_ticks() - start_time) / 1000
-        if elapsed_time >= 5:
+        if elapsed_time >= 2:
             print("10 seconds have passed! Changing the wind!")
             [fx, fy] = generateForces()
             print("Forta pe x: ", fx, "\n", "Forta pe y: ", fy, "\n")
